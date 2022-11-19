efdbe4da3cf2ee44402fcad264c32d556bd8cd2f
hsuyihnin <108940178+hsuyihnin@users.noreply.github.com>
2022-11-19 23:21:07 +0630

    prob solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useReducer, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 const categories = [
   {
@@ -14,80 +15,107 @@ const categories = [
   },
 ];
 
-function productReducer(state, action) {
-  switch (action.type) {
-    case "SET_PRODUCT":
-      return {
-        ...state,
-        data: action.payload,
-        isLoading: false,
-      };
-    case "SELECT_CATEGORY":
-      return {
-        ...state,
-        data: state.data.filter(
-          (product) => product.category === action.payload
-        ),
-        isLoading: false,
-      };
-    case "FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        hasError: true,
-      };
-    default:
-      throw new Error();
-  }
-}
+// function productReducer(state, action) {
+//   switch (action.type) {
+//     case "SET_PRODUCT":
+//       return {
+//         ...state,
+//         data: action.payload,
+//         isLoading: false,
+//       };
+//     case "FAILURE":
+//       return {
+//         ...state,
+//         isLoading: false,
+//         hasError: true,
+//       };
+//     default:
+//       throw new Error();
+//   }
+// }
 
 export default function App() {
-  const [products, dispatch] = useReducer(productReducer, {
+  // const [products, dispatch] = useReducer(productReducer, {
+  //   data: [],
+  //   isLoading: true,
+  //   hasError: false,
+  // });
+  const [products, setProducts] = useState({
     data: [],
     isLoading: true,
     hasError: false,
   });
+  const [filteredProducts, setfilteredProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isSelected, setIsSelected] = useState(false);
+
   useEffect(() => {
-    fetch(" http://localhost:3000/products")
+    fetch("http://localhost:3000/products")
       .then((res) => res.json())
-      .then((data) =>
-        dispatch({
-          type: "SET_PRODUCT",
-          payload: data,
-        })
-      )
-      .catch(() => dispatch({ type: "FAILURE" }));
-  }, [products]);
+      .then((data) => {
+        setProducts({
+          ...products,
+          isLoading: false,
+          data: data,
+        });
+        setIsSelected(false);
+      })
+      .catch(() => setProducts({ ...products, hasError: true }));
+  }, []);
+
+  useEffect(() => {
+    setfilteredProducts(
+      products.data.filter((product) => product.category === selectedCategory)
+    );
+    setIsSelected(true);
+  }, [selectedCategory]);
 
   return (
     <div className="flex">
       <div className="sidebar w-2/12">
         <div className="category">
           <h2>Product's Category</h2>
-          {categories.map((category) => {
-            return (
-              <div
+          <ul>
+            {categories.map((category) => (
+              <li
                 key={category.id}
-                onClick={() => {
-                  dispatch({ type: "SELECT_CATEGORY", payload: category.id });
-                }}
-                className="hover:cursor-pointer bg-slate-400"
+                onClick={() => setSelectedCategory(category.id)}
+                className="hover:cursor-pointer bg-slate-400 hover:bg-slate-300 py-2 text-center border-b border-gray-100"
               >
                 {category.name}
-              </div>
-            );
-          })}
+              </li>
+            ))}
+          </ul>
         </div>
         {/* <div className="filtered"></div> */}
       </div>
-      <div className="list w-10/12 columns-sm">
+      <div className="list w-10/12 flex flex-wrap justify-center gap-10">
         {products.isLoading ? (
           <p>Loading...</p>
+        ) : isSelected ? (
+          filteredProducts.map((product) => {
+            return (
+              <div key={product.id} className="product w-60">
+                <img
+                  className="w-60 h-64 image m-0"
+                  src={product.image}
+                  alt=""
+                />
+                <h3 className="name">{product.name}</h3>
+
+                <span>{product.price}</span>
+              </div>
+            );
+          })
         ) : (
           products.data.map((product) => {
             return (
-              <div key={product.id} className="product w-60 mr-0">
-                <img className="w-60 image m-0" src={product.image} alt="" />
+              <div key={product.id} className="product w-60">
+                <img
+                  className="w-60 h-64 image m-0"
+                  src={product.image}
+                  alt=""
+                />
                 <h3 className="name">{product.name}</h3>
 
                 <span>{product.price}</span>
@@ -95,6 +123,7 @@ export default function App() {
             );
           })
         )}
+
         {products.hasError && <p>Something went wrong</p>}
       </div>
     </div>
