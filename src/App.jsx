4290368086169dce73ff9d1f729f91fc48bd2496
hsuyihnin <108940178+hsuyihnin@users.noreply.github.com>
2022-11-21 20:51:10 +0630

    filtered by year

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,22 @@
-import React, { useState } from 'react';
-import { useReducer, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import ProductList from './components/ProductList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
 
 const categories = [
   {
     id: 1,
-    name: 'Album',
+    name: "Album",
   },
-  { id: 2, name: 'DVD' },
+  { id: 2, name: "DVD" },
+
   {
     id: 3,
-    name: 'BT21',
+    name: "BT21",
   },
 ];
-
-// function productReducer(state, action) {
-//   switch (action.type) {
-//     case "SET_PRODUCT":
-//       return {
-//         ...state,
-//         data: action.payload,
-//         isLoading: false,
-//       };
-//     case "FAILURE":
-//       return {
-//         ...state,
-//         isLoading: false,
-//         hasError: true,
-//       };
-//     default:
-//       throw new Error();
-//   }
-// }
+const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022];
 
 export default function App() {
-  // const [products, dispatch] = useReducer(productReducer, {
-  //   data: [],
-  //   isLoading: true,
-  //   hasError: false,
-  // });
   const [products, setProducts] = useState({
     data: [],
     isLoading: true,
@@ -48,10 +24,13 @@ export default function App() {
   });
   const [filteredProducts, setfilteredProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedYear, setSelectedYear] = useState(null);
+  const [filteredProductsByYear, setFilteredProductsByYear] = useState(null);
+  const [renderedProducts, setRenderedProducts] = useState([]);
   const [isSelected, setIsSelected] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:3000/products')
+    fetch("http://localhost:3000/products")
       .then((res) => res.json())
       .then((data) => {
         setProducts({
@@ -71,34 +50,61 @@ export default function App() {
     setIsSelected(true);
   }, [selectedCategory]);
 
+  useEffect(() => {
+    setFilteredProductsByYear(
+      renderedProducts.filter((product) => product.year === selectedYear)
+    );
+
+    console.log( renderedProducts );
+    console.log(filteredProductsByYear)
+  }, [selectedYear, renderedProducts]);
   return (
-    <div className='flex'>
-      <div className='sidebar w-2/12'>
-        <div className='category'>
+    <div className="flex">
+      <div className="sidebar w-2/12">
+        <div className="category">
           <h2>Product's Category</h2>
           <ul>
             {categories.map((category) => (
               <li
                 key={category.id}
                 onClick={() => setSelectedCategory(category.id)}
-                className='hover:cursor-pointer bg-slate-400 hover:bg-slate-300 py-2 text-center border-b border-gray-100'
+                className="hover:cursor-pointer bg-slate-400 hover:bg-slate-300 py-2 text-center border-b border-gray-100"
               >
                 {category.name}
               </li>
             ))}
           </ul>
         </div>
-        {/* <div className="filtered"></div> */}
+        <div className="year">
+          <h2>Filtered by :</h2>
+          <ul>
+            {years.map((year) => {
+              return (
+                <li
+                  key={year}
+                  onClick={() => {
+                    setSelectedYear(year);
+                  }}
+                  className="hover:cursor-pointer bg-slate-400 hover:bg-slate-300 py-2 text-center border-b border-gray-100"
+                >
+                  {year}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <div className='list w-10/12 flex flex-wrap justify-center gap-10'>
+      <div className="list w-10/12 flex flex-wrap justify-center gap-10">
         {products.isLoading && <p>Loading...</p>}
 
         <ProductList
           isSelected={isSelected}
           filteredProducts={filteredProducts}
           products={products}
+          filteredProductsByYear={filteredProductsByYear}
+          setRenderedProducts={setRenderedProducts}
         />
-        
+
         {products.hasError && <p>Something went wrong</p>}
       </div>
     </div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const ProductList = ({ isSelected, filteredProducts, products }) => {
+const ProductList = ({
+  isSelected,
+  filteredProducts,
+  products,
+  setRenderedProducts,
+  filteredProductsByYear,
+
+}) => {
   const productsToRender = isSelected ? filteredProducts : products.data;
+  useEffect(() => {
+    const renderedProducts = productsToRender;
+    setRenderedProducts(renderedProducts);
+  }, [productsToRender]);
+
+  {
+    // isFiltered &&
+    //   filteredProductsByYear.map((product) => (
+    //     <div key={product.id} className="product w-60">
+    //       <img className="w-60 h-64 image m-0" src={product.image} alt="" />
+    //       <h3 className="name">{product.name}</h3>
+
+    //       <span>{product.price}</span>
+    //     </div>
+    //   ));
+  }
 
   return productsToRender.map((product) => (
-    <div key={product.id} className='product w-60'>
-      <img className='w-60 h-64 image m-0' src={product.image} alt='' />
-      <h3 className='name'>{product.name}</h3>
+    <div key={product.id} className="product w-60">
+      <img className="w-60 h-64 image m-0" src={product.image} alt="" />
+      <h3 className="name">{product.name}</h3>
 
       <span>{product.price}</span>
     </div>
