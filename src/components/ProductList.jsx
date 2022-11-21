0d253d32741513112baa843860e8dc920ae709fc
hsuyihnin <108940178+hsuyihnin@users.noreply.github.com>
2022-11-21 21:58:22 +0630

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,20 +13,7 @@ const ProductList = ({
     const renderedProducts = productsToRender;
     setRenderedProducts(renderedProducts);
   }, [productsToRender]);
-
-  {
-    // isFiltered &&
-    //   filteredProductsByYear.map((product) => (
-    //     <div key={product.id} className="product w-60">
-    //       <img className="w-60 h-64 image m-0" src={product.image} alt="" />
-    //       <h3 className="name">{product.name}</h3>
-
-    //       <span>{product.price}</span>
-    //     </div>
-    //   ));
-  }
-
-  return productsToRender.map((product) => (
+ return productsToRender.map((product) => (
     <div key={product.id} className="product w-60">
       <img className="w-60 h-64 image m-0" src={product.image} alt="" />
       <h3 className="name">{product.name}</h3>
